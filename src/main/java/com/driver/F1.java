dcd56871999b356ec7e464a9ba5646d891ce5bfd
Pratibha <pratibha.sept19@gmail.com>
2025-04-16 01:48:14 +0530

    Vehicle-Assignment

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -3,7 +3,7 @@ package com.driver;
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
-        super(name, 40, 45, 4, 4, 6, isManual, "con", 7);
+        super( 4, 4, 6, isManual, "con", 7);
 
         //Use arbitrary values for parameters which are not mentioned
     }
